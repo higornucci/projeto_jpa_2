229416cce7441354b9e3da0c197e5bc1854fea08
Higor Nucci <higornucci@gmail.com>
2017-08-15 14:50:55 -0400

    Inserção do uso de criteria para o filtro de produtos

--- src/main/java/br/com/caelum/dao/ProdutoDao.java
+++ src/main/java/br/com/caelum/dao/ProdutoDao.java
@@ -6,12 +6,9 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Path;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 
+import br.com.caelum.model.Categoria;
 import org.springframework.stereotype.Repository;
 
 import br.com.caelum.model.Loja;
@@ -28,37 +25,39 @@ public class ProdutoDao {
 	}
 
 	public Produto getProduto(Integer id) {
-		Produto produto = em.find(Produto.class, id);
-		return produto;
+		return em.find(Produto.class, id);
 	}
 
 	public List<Produto> getProdutos(String nome, Integer categoriaId, Integer lojaId) {
-		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-		CriteriaQuery<Produto> query = criteriaBuilder.createQuery(Produto.class);
-		Root<Produto> root = query.from(Produto.class);
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<Produto> query = builder.createQuery(Produto.class);
+		Root<Produto> produtoRoot = query.from(Produto.class);
 
-		Path<String> nomePath = root.<String> get("nome");
-		Path<Integer> lojaPath = root.<Loja> get("loja").<Integer> get("id");
-		Path<Integer> categoriaPath = root.join("categorias").<Integer> get("id");
-
-		List<Predicate> predicates = new ArrayList<>();
+		Predicate conjuncao = builder.conjunction();
 
 		if (!nome.isEmpty()) {
-			Predicate nomeIgual = criteriaBuilder.like(nomePath, nome);
-			predicates.add(nomeIgual);
+			Path<String> nomeProduto = produtoRoot.get("nome");
+			Predicate nomeIgual = builder.like(nomeProduto, "%" + nome + "%");
+
+			conjuncao = builder.and(nomeIgual);
 		}
+
 		if (categoriaId != null) {
-			Predicate categoriaIgual = criteriaBuilder.equal(categoriaPath, categoriaId);
-			predicates.add(categoriaIgual);
+			Join<Produto, List<Categoria>> join = produtoRoot.join("categorias");
+			Path<Integer> categoriaProduto = join.get("id");
+
+			conjuncao = builder.and(conjuncao,
+					builder.equal(categoriaProduto, categoriaId));
 		}
+
 		if (lojaId != null) {
-			Predicate lojaIgual = criteriaBuilder.equal(lojaPath, lojaId);
-			predicates.add(lojaIgual);
+			Path<Loja> loja = produtoRoot.get("loja");
+			Path<Integer> id = loja.get("id");
+
+			conjuncao = builder.and(conjuncao, builder.equal(id, lojaId));
 		}
 
-		query.where((Predicate[]) predicates.toArray(new Predicate[0]));
-
-		TypedQuery<Produto> typedQuery = em.createQuery(query);
+		TypedQuery<Produto> typedQuery = em.createQuery(query.where(conjuncao));
 		return typedQuery.getResultList();
 
 	}
